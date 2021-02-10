78d3a37d72c153f16997df371ab2478b80617057
Nicola Benaglia <nbenaglia@cuebiq.com>
2021-02-10 11:00:35 +0100

    Enable bucket versioning

--- lib/s3-stack.ts
+++ lib/s3-stack.ts
@@ -7,7 +7,8 @@ export class S3Stack extends cdk.Stack {
 
     const bucket = new s3.Bucket(this, 'nbenaglia-bucket', {
       encryption: s3.BucketEncryption.UNENCRYPTED,
-      removalPolicy: cdk.RemovalPolicy.DESTROY
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      versioned: true
     });
   }
 }
